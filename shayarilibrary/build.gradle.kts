df33c41b0d4bb7fa70b4ace882ee694dbb3d955d
aklearning <akproject2019@gmail.com>
2026-07-18 19:53:51 +0530

    Added release logic in shayari library

--- shayarilibrary/build.gradle.kts
+++ shayarilibrary/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.library)
+    id("maven-publish")
 }
+
+group = "com.github.akproject2019"
+version = "1.0.0"
 
 android {
     namespace = "com.example.shayarilibrary.sdk"
@@ -16,6 +20,9 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
+    publishing {
+        singleVariant("release")
+    }
 }
 
 dependencies {
@@ -26,4 +33,18 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.akproject2019"
+                artifactId = "shayarilibrary"
+                version = "1.0.0"
+            }
+        }
+    }
 }
